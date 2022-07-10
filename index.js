b1b995910294996e61e1d8b6009680c69f06b2f6
Hamid <www.abdulhamid3311@gmail.com>
2022-07-10 15:36:45 +0600

    added header

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ function display(number) {
     output.value += number
 }
 
+/* function parcent(number) {
+    output.value = output.value / 100
+}; */
+
 function calculate() {
     try {
         output.value = eval(output.value);
     } catch (err) {
         alert("invalid Number")
     }
-}
+};
+
+//Clear 
 function clr() {
     output.value = "";
-}
+};
+
+//Delete One Number
 function del() {
     output.value = output.value.slice(0, -1);
-}
+};
